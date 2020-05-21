8c6ffb914e22afbe4c0fedfa108e59be525e31f4
Anton <agafonov8@gmail.com>
2020-05-21 18:34:20 +0300

    Add label

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Temuulen Sukhbat on 21.05.2020.
+//  Copyright © 2020 Anton Agafonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
